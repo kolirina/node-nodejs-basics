be7c5c516a677fd591e475034bed01d76f6e263d
kolirina <irinairin@list.ru>
2024-10-03 17:58:14 +0300

    feat: add delete, list, rename and read to fs folder

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,15 @@
+import { join } from "path";
+import { promises as fs } from "fs";
+
 const list = async () => {
-    // Write your code here 
+  const path = join(import.meta.dirname, "./files");
+  try {
+    await fs.access(path);
+    const files = await fs.readdir(path);
+    console.log(files);
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await list();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,23 @@
+import { promises as fs } from "fs";
+import { join } from "path";
+
 const rename = async () => {
-    // Write your code here 
+  const wrongFile = join(import.meta.dirname, "./files/wrongFilename.txt");
+  const properFile = join(import.meta.dirname, "./files/properFilename.md");
+  try {
+    await fs.access(wrongFile);
+    try {
+      await fs.access(properFile);
+      throw new Error("FS operation failed");
+    } catch (error) {
+      if (error.code !== "ENOENT") {
+        throw error;
+      }
+    }
+    await fs.rename(wrongFile, properFile);
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await rename();
