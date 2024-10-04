ca76b0930519fbda3522e2634c693c330b08ea51
kolirina <irinairin@list.ru>
2024-10-04 21:17:44 +0300

    feat: add code to streams folder

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import { join } from "path";
+import { createWriteStream } from "fs";
 const write = async () => {
-    // Write your code here 
+  const filePath = join(import.meta.dirname, "./files/fileToWrite.txt");
+
+  const writeStream = createWriteStream(filePath);
+
+  process.stdin.pipe(writeStream);
+
+  writeStream.on("finish", () => {
+    process.exit();
+  });
+
+  writeStream.on("error", (err) => {
+    console.error("FS operation failed:", err.message);
+  });
 };
 
 await write();
