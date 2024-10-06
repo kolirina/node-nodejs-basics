35118ea73bb14252edc42a33566fb0355f270daf
kolirina <irinairin@list.ru>
2024-10-06 19:31:13 +0300

    fix: fix read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,13 +1,18 @@
 import { createReadStream } from "fs";
-import { join } from "path";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const read = async () => {
-  const filePath = join(import.meta.dirname, "./files/fileToRead.txt");
+  const filePath = join(__dirname, "./files/fileToRead.txt");
+  console.log(filePath);
 
   const readableStream = createReadStream(filePath, "utf-8");
 
   readableStream.on("data", (chunk) => {
-    process.stdout.write(chunk);
+    process.stdout.write(chunk + "\n");
   });
 
   readableStream.on("error", (err) => {
